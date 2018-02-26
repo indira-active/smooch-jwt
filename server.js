522c052b3bfdda062a9b6b2c43be86c7308559ae
Andrew Williams <andrewwilliams1995@Andrews-MacBook-Pro.local>
2018-02-26 06:34:29 +0100

    fix port

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 env(__dirname + '/.env');
 const http = require('http')
-
 const KEY_ID = process.env.KEY_ID;
 const SECRET = process.env.SECRET;
-const PORT = '8080';
+const PORT = 8080;
 const u = require('./utility')
 const signJwt = require('./jwt');
 
@@ -30,9 +29,10 @@ app.get('/', (req,res)=>{
 
 	});
 
-const port = u.normalizePort(process.env.PORT || PORT);
-app.set('port',port);
+const port = process.env.PORT || PORT;
+app.listen(port);
+/*app.set('port',port);
 const server = http.createServer(app);
 server.listen(PORT);
 server.on('error', u.onError);
-server.on('listening', ()=>u.onListening(server));
+server.on('listening', ()=>u.onListening(server));*/
