77acc5ecaed9bc8301a9ebb2313d4dfdfc6582b5
Nathan Guenther <nathanpguenther@gmail.com>
2018-03-10 23:19:22 +0000

    Fix code style

--- jwt/test.js
+++ jwt/test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 const nJwt = require('njwt')
 describe('index',function(){
 //	  describe('Jwt1', function(){
-//	    const testCase = 'awilliams1995';
+//	    const testCase = "awilliams1995";
 //	    it(`should return a particular string with the user ${testCase}`, function(){
 //	      const actualValue = signJwt(testCase);
 //	      const verified = nJwt.verify(actualValue,process.env.SECRET);
@@ -12,7 +12,7 @@ describe('index',function(){
 //	    });
 //	});
 //	  describe('Jwt2', function(){
-//	    const testCase = 'test2';
+//	    const testCase = "test2";
 //	    it(`should return a particular string with the user ${testCase}`, function(){
 //	      const actualValue = signJwt(testCase);
 //	      const verified = nJwt.verify(actualValue,process.env.SECRET);
@@ -20,19 +20,19 @@ describe('index',function(){
 //	    });
 //	});
 //	  describe('Jwt3', function(){
-//	    const testCase = 'test3';
+//	    const testCase = "test3";
 //	    it(`should return a particular string with the user ${testCase}`, function(){
 //	      const actualValue = signJwt(testCase);
 //	      const verified = nJwt.verify(actualValue,process.env.SECRET);
 //	     return assert.deepEqual(testCase,verified.body.userId)
 //	    });
 //	});
-	  describe('Jwt4', function(){
-	    const testCase = 'test4';
-	    it(`${testCase} should not equal the verified body of a jwt signed with the userId jwjwjw`, function(){
-	      const actualValue = err;
-	      const verified = nJwt.verify(actualValue,process.env.SECRET);
-			 return assert.notEqual(testCase,verified.body.userId)
+    describe('Jwt4', function(){
+        const testCase = "test4";
+        it(`${testCase} should not equal the verified body of a jwt signed with the userId jwjwjw`, function(){
+            const actualValue = err;
+            const verified = nJwt.verify(actualValue,process.env.SECRET);
+            return assert.notEqual(testCase,verified.body.userId)
 	    });
 	});
 });
