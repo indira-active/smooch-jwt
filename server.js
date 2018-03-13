a54fc2fae0151109c6c9cce3300ba7c2cdc3119f
Nathan Guenther <nathanpguenther@gmail.com>
2018-03-13 05:41:47 +0000

    Remove unused dependency

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const env = require('node-env-file');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -8,9 +7,6 @@ const cors = require('cors');
 const http = require('http')
 const PORT = 8080;
 const signJwt = require('./jwt');
-// if(process.env.NODE_ENV !== 'production'){
-// 	env(__dirname + '/.env')
-// }
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
